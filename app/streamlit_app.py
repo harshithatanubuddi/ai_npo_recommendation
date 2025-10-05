591e5a131f02692ff443bebcfa7bc171290184ae
Harshitha Tanubuddi <harshitha4569@gmail.com>
2025-10-05 22:37:18 +0530

    streamlit_app.py

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -1,4 +1,6 @@
+import sys
 import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 import streamlit as st
 import pandas as pd
 from src.donor_recommender_ml import recommend_for_donor
